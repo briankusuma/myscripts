d28acf74daf4da0ca6dd12ece81460f81893d13a
briankusuma <kusuma.brian@gmail.com>
2025-03-03 11:28:39 +0700

    innit

--- assets/membership.js
+++
@@ -1,8 +0,0 @@
-const formLogin = document.getElementById('login-cont');
-const formRecover = document.getElementById('recover-cont');
-const btnRecover = document.getElementById('btn-recover');
-
-btnRecover.addEventListener('click', () => {
-  formLogin.classList.remove('form-active');
-  formRecover.classList.add('form-active');
-})
