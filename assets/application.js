2670eed7940beb40ebd793b41827123d945449f8
briankusuma <kusuma.brian@gmail.com>
2025-03-06 12:05:26 +0700

    build: section begin

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,12 @@
 // Put your application javascript here
+console.log("hallo log")
+
+const slider = document.querySelector(".slider");
+
+function updateSliderBackground() {
+    const value = slider.value;
+    slider.style.setProperty("--progress", `${value}%`);
+}
+
+slider.addEventListener ("input", updateSliderBackground);
+updateSliderBackground(); // Inisialisasi pertama kali
