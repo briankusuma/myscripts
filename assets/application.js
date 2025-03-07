82ff84efae25c369f837eefa37e5f1afcb81475b
briankusuma <kusuma.brian@gmail.com>
2025-03-07 16:24:54 +0700

    build: section faq

--- assets/application.js
+++ assets/application.js
@@ -1,12 +1,38 @@
 // Put your application javascript here
 console.log("hallo log")
+document.addEventListener("DOMContentLoaded", () => {
+    const faqItems = document.querySelectorAll(".faq-item");
+    console.log("hallo log")
+    faqItems.forEach((item) => {
+        const button = item.querySelector(".faq-question");
 
-const slider = document.querySelector(".slider");
+        button.addEventListener("click", () => {
+            // Tutup semua FAQ sebelum membuka yang baru
+            faqItems.forEach((otherItem) => {
+                if (otherItem !== item) {
+                    otherItem.classList.remove("active");
+                    console.log("hallo log")
+                }
+            });
 
-function updateSliderBackground() {
-    const value = slider.value;
-    slider.style.setProperty("--progress", `${value}%`);
-}
+            // Toggle class 'active' hanya untuk item yang diklik
+            item.classList.toggle("active");
+        });
+    });
+});
 
-slider.addEventListener ("input", updateSliderBackground);
-updateSliderBackground(); // Inisialisasi pertama kali
+
+// const slider = document.querySelector(".slider");
+
+// function updateSliderBackground() {
+//     const value = slider.value;
+//     slider.style.setProperty("--progress", `${value}%`);
+// }
+
+// slider.addEventListener ("input", updateSliderBackground);
+// updateSliderBackground();
+
+
+
+
+
