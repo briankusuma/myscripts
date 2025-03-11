b275dd190e692c8e7aa44b8cc34444060cc18cf9
briankusuma <kusuma.brian@gmail.com>
2025-03-11 16:08:34 +0700

    build: addining testimonial slider, fixing: bug

--- assets/application.js
+++ assets/application.js
@@ -1,37 +1,46 @@
 // Put your application javascript here
-
 console.log("hallo log")
 document.addEventListener("DOMContentLoaded", () => {
     const faqItems = document.querySelectorAll(".faq-item");
     console.log("hallo log")
     faqItems.forEach((item) => {
         const button = item.querySelector(".faq-question");
-
         button.addEventListener("click", () => {
-            // Tutup semua FAQ sebelum membuka yang baru
             faqItems.forEach((otherItem) => {
                 if (otherItem !== item) {
                     otherItem.classList.remove("active");
                     console.log("hallo log")
                 }
             });
-
-            // Toggle class 'active' hanya untuk item yang diklik
             item.classList.toggle("active");
         });
     });
 });
 
 
-// const slider = document.querySelector(".slider");
+$(document).ready(function () {
+    $("#review-slider").slick({
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerPadding: "40px",
+    });
 
-// function updateSliderBackground() {
-//     const value = slider.value;
-//     slider.style.setProperty("--progress", `${value}%`);
-// }
+    $("#review-slider").on("afterChange", function (event, slick, currentSlide) {
+        var $activeDot = $(".slick-dots li").eq(currentSlide);
+        $(".slick-dots li").removeClass("active-progress");
+        $activeDot.addClass("active-progress");
+        $activeDot.css(
+            "--progress-duration",
+            slick.options.autoplaySpeed / 1000 + "s"
+        );
+    });
+    $("#review-slider").slick("slickGoTo", 0, true);
 
-// slider.addEventListener ("input", updateSliderBackground);
-// updateSliderBackground();
+});
 
 
 
